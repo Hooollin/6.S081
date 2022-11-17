5ec28d77b80039ebdb68f6a4e010eeee2303b6ed
Hollin <hollinisme@gmail.com>
2022-11-17 23:43:23 +0800

    feat: add trace

--- kernel/proc.c
+++ kernel/proc.c
@@ -282,6 +282,9 @@ fork(void)
 
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
+  
+  // copy syscall trace mask
+  np->tmask = p->tmask;
 
   // increment reference counts on open file descriptors.
   for(i = 0; i < NOFILE; i++)
